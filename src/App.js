307c08d7071496085d9361605358124da8309d9e
Zodak <ojepaul4jesus@gmail.com>
2022-11-13 13:20:20 +0100

    trash

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import Index from "./pages/Home";
+<<<<<<< HEAD
+=======
+import { Route, Routes } from "react-router-dom";
+import PlaceToStay from "./pages/PlaceToStay/PlaceToStay";
+>>>>>>> parent of 34d4357... nil
 function App() {
   return (
     <div className="App">
