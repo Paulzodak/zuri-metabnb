ffaef18dfdc2401148fd0f9f4fadd266b915d8a9
Zodak <ojepaul4jesus@gmail.com>
2022-11-13 13:18:52 +0100

    trash

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import Index from "./pages/Home";
-import { Route, Routes } from "react-router-dom";
-import PlaceToStay from "./pages/PlaceToStay/PlaceToStay";
 function App() {
   return (
     <div className="App">
